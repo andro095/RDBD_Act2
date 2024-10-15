9a6c7587ccbd3fa3238c32ff92d6deabcb8251ab
andro095 <andre.rodriguezgt@yahoo.com>
2024-10-14 23:50:22 -0400

    Finished

--- Amalgamated_Big_Co.sql
+++ Amalgamated_Big_Co.sql
@@ -176,3 +176,50 @@ CREATE TABLE DiscountApplied (
     CONSTRAINT PK_DiscountApplied PRIMARY KEY (DiscountID),
     CONSTRAINT FK_Product_Discount FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
 );
+
+INSERT HR.Employee (EmployeeID, lastname, firstname, title, hiredate, EmployeeAddress, EmployeeCity, EmployeePostalCode, EmployeeCountry, phone, EmployeeEmail, EmployeePhoneNo)VALUES
+(1, N'Davis', N'sara', N'CEO', CAST(N'2018-09-21 00:00:00.000' AS DateTime), N'212 - 29th ave', N'seatle', N'231', N'USA', N'(122) 212 2121', 'example@gmail.com', '1234567890'),
+
+(2, N'John', N'deo', N'manger', CAST(N'2018-09-21 00:00:00.000' AS DateTime), N'506 - 206 lester', N'waterloo', N'231', N'USA', N'(226) 565 8295', 'example@gmail.com', '1234567890'),
+
+(3, N'Wyatt', N'Abreu', N'Certified financial planner', N'2018-06-02 00:00:00', N'788-65 Westmount', N'WA', N'N2l', N'CA', N'(226) 784 9865', 'example@gmail.com', '1234567890'),
+
+(4, N'Steve', N'Arai', N'Chartered wealth manager', N'2017-07-05 00:00:00', N'23847 36A Ave Langley BC', N'WA', N'V2Z', N'CA', N'(604) 533-2340', 'example@gmail.com', '1234567890'),
+
+(5, N'Elenora', N'Smiley', N'Service desk analyst.', N'2015-06-05 00:00:00', N'43 Connor Lane Guelph ON', N'WA', N'N1E', N'CA', N'(519) 824-2661', 'example@gmail.com', '1234567890'),
+
+(6, N'Kenisha', N'Yeung', N'Chartered wealth manager', N'2012-11-25 00:00:00', N'32-441 Weber', N'WA', N'N6g', N'CA', N'(226) 484 6532', 'example@gmail.com', '1234567890'),
+(7, N'Nikia', N'Curtin', N'Network administrator', N'2010-06-09 00:00:00', N'39 Dobbin Lane Kanata ON', N'WA', N'K2M', N'CA', N'(613) 591-0347', 'example@gmail.com', '1234567890'),
+(8, N'Shaunte', N'Romaine', N'Network engineer', N'2013-07-08 00:00:00', N'733 Skene Rd Gilmour ON', N'WA', N'K0L', N'CA', N'(613) 474-1027', 'example@gmail.com', '1234567890'),
+(9, N'Quinn', N'Moorehead', N'Network architect', N'2014-08-05 00:00:00', N'1470 Pennyfarthing Dr 203 Vancouver BC', N'WA', N'V6J', N'CA', N'(604) 736-2823', 'example@gmail.com', '1234567890'),
+(10, N'Idell', N'Muck', N'Network manager', N'2014-09-09 00:00:00', N'452-2750 Fairlane St , Abbotsford, BC', N'WA', N'V2S', N'CA', N'604-870-9982', 'example@gmail.com', '1234567890');
+
+INSERT INTO Sales.Customer(CustomerID, CustomerName, CustomerEmail, RepeatCustomer)
+VALUES
+(1, N'360Networks Inc.', 'example@gmail.com', 1),
+(2, N'3DLabs Inc. Ltd.', 'example@gmail.com', 1),
+(3, N'724 Solutions Inc.', 'example@gmail.com', 1),
+(4, N'Acme Corporation', 'example@gmail.com', 1),
+(5, N'Globex Corporation', 'example@gmail.com', 1),
+(6, N'Soylent Corp', 'example@gmail.com', 1),
+(7, N'Initech', 'example@gmail.com', 1),
+(8, N'Umbrella Corporation', 'example@gmail.com', 1),
+(9, N'Hooli', 'example@gmail.com', 1),
+(10, N'Massive Dynamic', 'example@gmail.com', 1);
+
+insert into Sales.Customer_Details(customerid, customeraddress, customercity, customerpostalcode, customercountry, customerphoneno, Customer_DetailsID)
+values
+(1, N'506-201 lester', N'WA', N'N2l', N'CA', N'(226) 565', 1),
+(2, N'456-84 King St', N'WA', N'N2l',N'CA', N'(226) 784', 2),
+(3, N'52-489 Albert St', N'WA', N'N6g',N'CA', N'(226) 852', 3),
+(4, N'7307 Prasmount Pl', N'BC', N'V0M',N'CA', N'604-796', 4),
+(5, N'1145 Dakota St 209', N'MB', N'N6g',N'CA', N'(204) 949', 5),
+(6, N'68 Baycrest Pl SW 5', N'AB', N'T2V',N'CA', N'(403) 244', 6),
+(7, N'PO Box 206', N'AB ', N'T0C',N'CA', N'780-696', 7),
+(8, N'513A Deer St', N'AB', N'T1L',N'CA', N'403-762', 8),
+(9, N'2-119 4 Ave W', N'AB', N'T0K',N'CA', N'403-545', 9),
+(10, N'42 Rue Bourbonnais', N'QC', N'J0P',N'CA', N'450-308', 10);
+
+
+
+
